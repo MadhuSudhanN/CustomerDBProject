616dc9a32932c1325e9670468a6778ebd4481415
Madhu sudhan N <127393737+MadhuSudhanN@users.noreply.github.com>
2026-03-26 10:51:21 +0530

    Create Emp.java

---
+++ CDBS/src/main/java/com/entity/Emp.java
@@ -0,0 +1,13 @@
+public class Emp{
+  private int id;
+  private String name;
+  private String dept;
+  public void setId(int id){
+  this.id=id;
+  }
+  public int getId()
+  {
+    return id;
+  }
+  
+}
